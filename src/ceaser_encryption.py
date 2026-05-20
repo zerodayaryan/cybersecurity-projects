127ca8d97ef20963bf6607260f3bb96c81bb3447
aryan01 <270636831+zerodayaryan@users.noreply.github.com>
2026-05-20 13:49:32 +0530

    update

--- src/ceaser_encryption.py
+++ src/ceaser_encryption.py
@@ -2,9 +2,14 @@ alphabets = [' ' ,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p
 text = input("Enter text ").lower()
 key = int(input("Enter key length "))
 cipher=''
+special=['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+numbers=str([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+index=0
 for char in text:
+   if char in special or char in numbers:
+    cipher+=char
+   else:
     index = (alphabets.index(char) + int(key)) % len(alphabets)
     cipher+=alphabets[index]
-
 
 print("cipher text = ",cipher)
